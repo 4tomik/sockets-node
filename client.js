5fdfdb57c8414145d28837dd1284011b94e26242
4tomik <4tomikyt@gmail.com>
2021-03-25 19:15:53 +0100

    Sockets Node Youtube

---
+++ client.js
@@ -0,0 +1,62 @@
+const { Socket } = require("net");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const END = "END";
+
+const error = (message) => {
+  console.error(message);
+  process.exit(1);
+};
+
+const connect = (host, port) => {
+  console.log(`Connecting to ${host}:${port}`);
+
+  const socket = new Socket();
+  socket.connect({ host, port });
+  socket.setEncoding("utf-8");
+
+  socket.on("connect", () => {
+    console.log("Connected");
+
+    readline.question("Choose your username: ", (username) => {
+      socket.write(username);
+      console.log(`Type any message to send it, type ${END} to finish`);
+    });
+
+    readline.on("line", (message) => {
+      socket.write(message);
+      if (message === END) {
+        socket.end();
+        console.log("Disconnected");
+        process.exit(0);
+      }
+    });
+
+    socket.on("data", (data) => {
+      console.log(data);
+    });
+  });
+
+  socket.on("error", (err) => error(err.message));
+};
+
+const main = () => {
+  if (process.argv.length !== 4) {
+    error(`Usage: node ${__filename} host port`);
+  }
+
+  let [, , host, port] = process.argv;
+  if (isNaN(port)) {
+    error(`Invalid port ${port}`);
+  }
+  port = Number(port);
+
+  connect(host, port);
+};
+
+if (module === require.main) {
+  main();
+}
---
+++ server.js
@@ -0,0 +1,72 @@
+const { Server } = require("net");
+
+const host = "0.0.0.0";
+const END = "END";
+
+const connections = new Map();
+
+const error = (message) => {
+  console.error(message);
+  process.exit(1);
+};
+
+const sendMessage = (message, origin) => {
+  for (const socket of connections.keys()) {
+    if (socket !== origin) {
+      socket.write(message);
+    }
+  }
+};
+
+const listen = (port) => {
+  const server = new Server();
+
+  server.on("connection", (socket) => {
+    const remoteSocket = `${socket.remoteAddress}:${socket.remotePort}`;
+    console.log(`New connection from ${remoteSocket}`);
+    socket.setEncoding("utf-8");
+
+    socket.on("data", (message) => {
+      connections.values();
+      if (!connections.has(socket)) {
+        console.log(`Username ${message} set for connection ${remoteSocket}`);
+        connections.set(socket, message);
+      } else if (message === END) {
+        console.log(`Connection with ${remoteSocket} closed`);
+        connections.delete(socket);
+        socket.end();
+      } else {
+        const fullMessage = `[${connections.get(socket)}]: ${message}`;
+        console.log(`${remoteSocket} -> ${fullMessage}`);
+        sendMessage(fullMessage, socket);
+      }
+    });
+
+    socket.on("error", (err) => error(err.message));
+  });
+
+  server.listen({ port, host }, () => {
+    console.log("Listening on port 8000");
+  });
+
+  server.on("error", (err) => error(err.message));
+};
+
+const main = () => {
+  if (process.argv.length !== 3) {
+    error(`Usage: node ${__filename} port`);
+  }
+
+  let port = process.argv[2];
+  if (isNaN(port)) {
+    error(`Invalid port ${port}`);
+  }
+
+  port = Number(port);
+
+  listen(port);
+};
+
+if (require.main === module) {
+  main();
+}
